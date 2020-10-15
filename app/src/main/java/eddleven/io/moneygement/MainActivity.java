e9656573a728b7cfd6a67ff26b1f8ce5d2627839
edd <dnr739528@gmail.com>
2020-10-15 11:04:32 +0800

    menambah SplashScreen activity dan tekan back 2x selama rentang waktu 2detik untuk keluar aplikasi

--- app/src/main/java/eddleven/io/moneygement/MainActivity.java
+++ app/src/main/java/eddleven/io/moneygement/MainActivity.java
@@ -3,12 +3,33 @@ package eddleven.io.moneygement;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.os.Handler;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
+
+    private Boolean mStatusBackPressed = false;
+    private final Handler mHandlerBackPressed = new Handler();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    @Override
+    public void onBackPressed() {
+        if(!this.mStatusBackPressed){
+            this.mStatusBackPressed = true;
+            Toast.makeText(this, "Tekan Kembali Sekali Lagi", Toast.LENGTH_SHORT).show();
+            this.mHandlerBackPressed.postDelayed(new Runnable() {
+                @Override
+                public void run() {
+                    mStatusBackPressed = false;
+                }
+            }, 2000);
+        } else {
+            super.onBackPressed();
+        }
     }
 }
