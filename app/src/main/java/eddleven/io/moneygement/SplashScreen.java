a3867832ec98912e097519efc078016058016631
edd <dnr739528@gmail.com>
2020-10-28 12:18:16 +0800

    perbaikan SplashScreen

--- app/src/main/java/eddleven/io/moneygement/SplashScreen.java
+++ app/src/main/java/eddleven/io/moneygement/SplashScreen.java
@@ -20,7 +20,7 @@ public class SplashScreen extends AppCompatActivity {
      * Some older devices needs a small delay between UI widget updates
      * and a change of the status and navigation bar.
      */
-    private static final int UI_ANIMATION_DELAY = 300;
+    private static final int UI_ANIMATION_DELAY = 0;
     private final Handler mHideHandler = new Handler();
     private View mContentView;
     private final Handler mFinishHandler = new Handler();
